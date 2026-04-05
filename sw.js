1a187f4d0956f82073d23a2ecdb1bf1bd1ec8a44
uelitonscs-netizen <uelitonscs@gmail.com>
2026-04-05 16:53:10 -0400

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,19 @@
+const CACHE = 'fazenda-v2';
+
+self.addEventListener('install', e => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+// Always fetch from network, no caching
+self.addEventListener('fetch', e => {
+  e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
+});
